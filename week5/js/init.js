53c4e3c91c1e3e78e92cb2fc93541ea6be4a2f75
penelopelamb <smileyfacelamb@gmail.com>
2022-05-03 23:19:06 -0700

    Lab 4

--- week5/js/init.js
+++ week5/js/init.js
@@ -1,6 +1,6 @@
 // JavaScript const variable declaration
 const map = L.map('the_map').setView([33.999859, -118.465607], 16.5); 
-
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQaqt_cytRgZLP_zz6bDh-Ye25FthzWdYWtAd1fWcqIv9YWrwJxaverrbaGmPUGrNGEblnyDcVvyT39/pub?output=csv" 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -10,12 +10,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
             
-saltnstraw = '<p>Salt and Straw is a handmade ice cream parlor and they change their special flavors monthly. They have vegan options too!</p>' + '<br /> Here is the ' + '<a href="https://saltandstraw.com/pages/flavors">menu</a>';
-wanderlust = '<p>Wanderlust has really unique flavors and never disappoints! They also just opened a location in Sawtelle!</p>' + '<br /> Here is the '+'<a href="https://wanderlustcreamery.com/menu">menu</a>';
-somisomi = '<p>SomiSomi has some of the tastiest soft serve I have had. I always order the ah-boong because the fish-shaped waffle cone makes it taste even better ;)</p>' + '<br /> Here is the ' + '<a href="https://www.somisomi.com/menu">menu</a>';
-milletcrepe = '<p>Millet Crepe serves their ice cream in a crepe rolled like a cone. When you go, you MUST order the creme brulee crepe.</p>' + '<br /> Here is the ' + '<a href="https://www.milletcrepe.com/#dYfCDj">menu</a>';
-dipnmix = '<p>Dip n Mix is the best late-night ice cream option for UCLA students. They are also probably one of the only places I know of that does not charge extra for acai bowl toppings</p>' + '<br /> They do not have a website, but here is their ' + '<a href="https://www.yelp.com/biz/dipnmix-los-angeles-6">Yelp</a>';
-saffronrose = '<p>Saffron and Rose serves delicious, creamy Persian Ice Cream. I always order the Saffron Pistachio and White Rose (which makes me basic because those are their most popular flavors).</p>' + '<br /> Here is their ' + '<a href="https://saffronrosepersianicecream.com/flavors/">menu</a>';
+const saltnstraw = '<p>Salt and Straw is a handmade ice cream parlor and they change their special flavors monthly. They have vegan options too!</p>' + '<br /> Here is the ' + '<a href="https://saltandstraw.com/pages/flavors">menu</a>';
+const wanderlust = '<p>Wanderlust has really unique flavors and never disappoints! They also just opened a location in Sawtelle!</p>' + '<br /> Here is the '+'<a href="https://wanderlustcreamery.com/menu">menu</a>';
+const somisomi = '<p>SomiSomi has some of the tastiest soft serve I have had. I always order the ah-boong because the fish-shaped waffle cone makes it taste even better ;)</p>' + '<br /> Here is the ' + '<a href="https://www.somisomi.com/menu">menu</a>';
+const milletcrepe = '<p>Millet Crepe serves their ice cream in a crepe rolled like a cone. When you go, you MUST order the creme brulee crepe.</p>' + '<br /> Here is the ' + '<a href="https://www.milletcrepe.com/#dYfCDj">menu</a>';
+const dipnmix = '<p>Dip n Mix is the best late-night ice cream option for UCLA students. They are also probably one of the only places I know of that does not charge extra for acai bowl toppings</p>' + '<br /> They do not have a website, but here is their ' + '<a href="https://www.yelp.com/biz/dipnmix-los-angeles-6">Yelp</a>';
+const saffronrose = '<p>Saffron and Rose serves delicious, creamy Persian Ice Cream. I always order the Saffron Pistachio and White Rose (which makes me basic because those are their most popular flavors).</p>' + '<br /> Here is their ' + '<a href="https://saffronrosepersianicecream.com/flavors/">menu</a>';
 
 addMarker(33.989052, -118.462440,'Salt n Straw', saltnstraw);
 addMarker(33.999859, -118.465607, 'Wanderlust', wanderlust);
@@ -24,11 +24,7 @@ addMarker(34.040489,-118.443588,'Millet Crepe',milletcrepe);
 addMarker(34.062620,-118.448190,'Dip n Mix', dipnmix);
 addMarker(34.055569,-118.441971, 'Saffron and Rose', saffronrose);
 
-//createButtons(33.999859, -118.465607,'Venice');
-//createButtons(34.039450,-118.442750, 'Sawtelle');
-//createButtons(34.062620,-118.448190, 'Westwood');
-
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQaqt_cytRgZLP_zz6bDh-Ye25FthzWdYWtAd1fWcqIv9YWrwJxaverrbaGmPUGrNGEblnyDcVvyT39/pub?output=csv" 
+loadData(dataUrl)
 
 function loadData(url){
     Papa.parse(url, {
@@ -66,4 +62,3 @@ function createButtons(lat,lng,title){
     document.getElementById("space_for_buttons").appendChild(newButton);
 }
 
-loadData(dataUrl)
